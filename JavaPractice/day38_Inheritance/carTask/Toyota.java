4903c8eaca3b6a2e53f42f65b49d0bb904b2356d
elvirka7 <elvirka7@gmail.com>
2022-01-13 11:00:06 -0500

    Created methods for Toyota class

--- JavaPractice/day38_Inheritance/carTask/Toyota.java
+++ JavaPractice/day38_Inheritance/carTask/Toyota.java
@@ -1,4 +1,13 @@
 package day38_Inheritance.carTask;
 
-public class Toyota {
+public class Toyota extends Car{
+
+    public Toyota(String brand, String model, int year, double price, String color, int miles) {
+        super("Toyota", model, year, price, color, miles);
+    }
+
+    public void reliable(){
+        System.out.println(brand+" "+model+" is relialbe");
+    }
+
 }
