e408199a3389b6e7c80fe21def3b697ff8e6b4d8
elvirka7 <elvirka7@gmail.com>
2022-01-13 22:28:13 -0500

    CarTask has final

--- JavaPractice/day38_Inheritance/carTask/Car.java
+++ JavaPractice/day38_Inheritance/carTask/Car.java
@@ -49,12 +49,27 @@ public class Car {
 
 }
 /*
-Create a class called Car
-			instance variables:
-				brand, model, year, price, color, miles
+carTask:
+    Create a class called Car
+        instance variables:
+            brand, model, year, price, color, miles
 
-				add a constructor to set all the fields
+            add a constructor to set all the fields
 
-			instance methods:
-				start(), drive(),toString()
+        instance methods:
+            start(), drive(),toString()
+
+    Create the following sub classes of Car:
+        1. Toyota:
+                extra methods:
+                    reliable()
+
+        2. BMW:
+                extra methods:
+                    breaksDown()
+                    racing()
+
+        3. Tesla:
+                extra methods:
+                    autoPilot();
  */
